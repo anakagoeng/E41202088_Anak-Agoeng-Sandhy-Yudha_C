934218c6f6bd5fbcdfb47b0a5eee1d21e1701405
Anak Agoeng Sandhy Yudha <agungsandy33@gmail.com>
2021-03-30 02:38:44 +0700

    Latihan Minggu ke-4

---
+++ Minggu 4/minggu04_latihan/latihan1/perulangan_while.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu04_latihan.latihan1;
+
+/**
+ *
+ * @author AGUNG
+ */
+public class perulangan_while {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i <= 10) {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        }
+
+    }
+}
