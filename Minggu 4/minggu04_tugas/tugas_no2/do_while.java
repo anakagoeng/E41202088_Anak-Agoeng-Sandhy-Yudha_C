420b2281593da1cf2985e119f4af189f72fe544f
Anak Agoeng Sandhy Yudha <agungsandy33@gmail.com>
2021-04-10 12:12:13 +0700

    Update do_while.java

--- Minggu 4/minggu04_tugas/tugas_no2/do_while.java
+++ Minggu 4/minggu04_tugas/tugas_no2/do_while.java
@@ -11,19 +11,27 @@ package minggu04_tugas.tugas_no2;
  */
 public class do_while {
     public static void main(String[] args){
-        int angka = 1;
-
+        int i = 1;
+        int j = 2;
+        
         System.out.println("         Do__While          ");
         System.out.println("Bilangan kelipatan 2 (1-100)");
         System.out.println("============================");
         
         do {
-            angka = angka * 2;
-            if (angka > 100){
-                break;
-            }
-            System.out.print(angka + "   ");
-        } while (angka < 100);
+            System.out.print(i + "   ");
+            i = i * 2;
+            i *= 2;
+        } while (i <= 100);
         System.out.println("");
+        System.out.println("Perkalian 2 tiap bilangan (1-100)");
+        System.out.println("============================");
+        
+        do {
+            System.out.print(j + "   ");
+            j += 2;
+        } while (j <= 100);
+        System.out.print("");
+        
     }
 }
